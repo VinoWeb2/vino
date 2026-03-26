52cfac2863570473827a589b4a6dbb3e6c984312
Greszta <00.Patrik.Greszta@gmail.com>
2026-03-26 10:36:23 -0400

    espace

--- caveo/database/migrations/2026_03_26_132845_create_inventaires_table.php
+++ caveo/database/migrations/2026_03_26_132845_create_inventaires_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('inventaires', function (Blueprint $table) {
-            $table->id();
+            $table->id(); 
             $table->unsignedBigInteger('id_cellier');
             $table->unsignedBigInteger('id_bouteille');
             $table->integer('quantite');
