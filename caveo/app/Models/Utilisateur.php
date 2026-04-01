212acc41d7c6aafe3a17a658b653530264411544
Greszta <00.Patrik.Greszta@gmail.com>
2026-04-01 15:21:07 -0400

    Ajout du modele Utilisateur avec champ mot_de_passe

--- caveo/app/Models/Utilisateur.php
+++ caveo/app/Models/Utilisateur.php
@@ -3,9 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Utilisateur extends Model
+class Utilisateur extends Authenticatable
 {
     use HasFactory;
+
+     protected $fillable = [
+        'prenom',
+        'nom',
+        'email',
+        'mot_de_passe',
+    ];
+
+    /**
+     * Indique quel champ utiliser comme mot de passe.
+     * Par défaut Laravel utilise "password", mais ici on utilise "mot_de_passe".
+     */
+    public function getAuthPassword()
+    {
+        return $this->mot_de_passe;
+    }
 }
