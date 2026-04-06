80b49038c5f9f284d56d91430b17bb631ffca909
DreamCurson <dreamcurson@outlook.com>
2026-04-06 13:00:51 -0400

    Finalisation de la page catalogue avec le style, l'image par défaut et les tailles

--- caveo/resources/views/catalogue/index.blade.php
+++ caveo/resources/views/catalogue/index.blade.php
@@ -34,30 +34,33 @@
 </div>
 
 @foreach($bouteilles as $bouteille)
-    <div class="flex gap-6 items-start m-4">
-        <img src="{{ $bouteille->image }}" alt="" class="w-16 h-auto object-contain">
+    <div class="flex gap-6 m-4 mb-6 font-roboto border p-4 rounded">
+        <div class="w-[90px] flex justify-center">
+            <img src="{{ $bouteille->image ?? asset('images/bouteille-vide.png') }}" alt="" class="w-auto h-[135px]">
+        </div>
 
-        <div>
-            <!-- Affichage du nom en plus grand -->
-            <h2 class="font-semibold text-lg">
-                {{ $bouteille->nom }}
-            </h2>
+        <div class="flex flex-col justify-between flex-1">
+            <div>
+                <h2 class="font-semibold text-lg">
+                    {{ $bouteille->nom }}
+                </h2>
 
-            <!-- Affichage en ligne du pays, du format et du type -->
-            <div class="flex items-center text-sm text-gray-600 space-x-2">
-                <p>{{ $bouteille->pays ?? "" }}</p>
-                <span>|</span>
-                <p>{{ $bouteille->format ?? "" }} ml</p>
-                <span>|</span>
-                <p>{{ $bouteille->type ?? "" }}</p>
+                <div class="flex items-center text-sm text-gray-600 space-x-2">
+                    <p>{{ $bouteille->pays ?? "" }}</p>
+                    <span>|</span>
+                    <p>{{ $bouteille->format ?? "" }} ml</p>
+                    <span>|</span>
+                    <p>{{ $bouteille->type ?? "" }}</p>
+                </div>
+
+                <p class="mt-2 font-medium mb-3">
+                    {{ $bouteille->prix ?? "Non spécifié" }} $
+                </p>
             </div>
 
-            <!-- Affichage du prix -->
-            <p class="mt-2 font-medium">
-                {{ $bouteille->prix ?? "Non spécifié" }} $
-            </p>
-
-            <a href="#" class="button">Détail</a>
+            <a href="#" class="px-4 py-2 bg-[#A83248] text-white rounded w-max">
+                Détail
+            </a>
         </div>
     </div>
 @endforeach
@@ -65,11 +68,11 @@
 <div class="flex justify-between items-center mx-auto my-5 mb-24">
     @if ($bouteilles->onFirstPage())
         <span>
-            <img src="{{ asset('images/fleches/gauche-gris.svg') }}" class="w-14" alt="">
+            <img src="{{ asset('images/fleches/gauche-gris.svg') }}" class="w-14" alt="gauche bloqué">
         </span>
     @else
         <a href="{{ $bouteilles->previousPageUrl() }}">
-            <img src="{{ asset('images/fleches/gauche-rouge.svg') }}" class="w-14" alt="">
+            <img src="{{ asset('images/fleches/gauche-rouge.svg') }}" class="w-14" alt="gauche">
         </a>
     @endif
 
@@ -77,11 +80,11 @@
 
     @if ($bouteilles->hasMorePages())
         <a href="{{ $bouteilles->nextPageUrl() }}">
-            <img src="{{ asset('images/fleches/droit-rouge.svg') }}" class="w-14" alt="">
+            <img src="{{ asset('images/fleches/droit-rouge.svg') }}" class="w-14" alt="droite">
         </a>
     @else
         <span>
-            <img src="{{ asset('images/fleches/droit-gris.svg') }}" class="w-14" alt="">
+            <img src="{{ asset('images/fleches/droit-gris.svg') }}" class="w-14" alt="droite bloqué">
         </span>
     @endif
 </div>
