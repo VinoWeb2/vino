0b8ea38aaa3b90477d5a41e398fe682e2715bca0
DreamCurson <dreamcurson@outlook.com>
2026-04-13 13:31:06 -0400

    Ajout de la function store avec erreur de validation

--- caveo/app/Http/Controllers/ListeAchatController.php
+++ caveo/app/Http/Controllers/ListeAchatController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\ListeAchat;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ListeAchatController extends Controller
 {
@@ -13,5 +15,24 @@ class ListeAchatController extends Controller
 
     public function create(){
         return view('liste-achat.create');
+    }
+
+    public function store(Request $request)
+    {
+        $utilisateur = Auth::user();
+
+        $validated = $request->validate([
+            'nom' => 'required|string|max:75|',
+            'description' => 'nullable|string|max:2000',
+        ]);
+
+        ListeAchat::create([
+            'nom' => $validated['nom'],
+            'id_utilisateur' => $utilisateur->id,
+            'description' => $validated['description'] ?? null,
+        ]);
+
+        return redirect()
+            ->route('achat.index');
     }
 }
