4b1b93666cf8f5cfe6f54307dfb112517af56c48
DreamCurson <dreamcurson@outlook.com>
2026-04-16 11:11:36 -0400

    Retrait de la flèche retour qui cause des erreurs

--- caveo/resources/views/liste-achat/index.blade.php
+++ caveo/resources/views/liste-achat/index.blade.php
@@ -1,13 +1,6 @@
 @extends('layouts.main')
 @section('title', 'Liste Achat')
 
-@section('fleche')
-<!-- Flèche de retour qui revient à la page précédente (Cellier ou Catalogue) permet de garder les filtres de catalogue si besoin -->
-<a href="{{ url()->previous() }}">
-    <img src="{{ asset('images/fleches/gauche-blanc.svg') }}" alt="Flèche de retour" class="w-10 h-10">
-</a>
-@endsection
-
 @section('content')
 <script type="module" src="{{ asset('js/liste-dropdown.js') }}"></script>
 <script type="module" src="{{ asset('js/message-flash-auto.js') }}"></script>
